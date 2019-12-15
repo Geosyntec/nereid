439500d96464a5d8c7aa005a7d817543fd4cab40
Austin Orr <austinmartinorr@gmail.com>
2019-12-14 18:17:34 -0800

    offline docs complete

--- nereid/nereid/main.py
+++ nereid/nereid/main.py
@@ -1,8 +1,36 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+from starlette.staticfiles import StaticFiles
 
 from nereid.api.api_v1.api import api_router
 from nereid.core import config
 
-app = FastAPI()
+app = FastAPI(title="nereid", docs_url=None, redoc_url=None)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+    )
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js",
+    )
+
 
 app.include_router(api_router, prefix=config.API_V1_STR)
