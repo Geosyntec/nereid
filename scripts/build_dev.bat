c74cc8b6b9b658d80bb7ea7ed72b5e75ef5a9640
Austin Orr <AOrr@geosyntec.com>
2020-01-10 17:12:22 -0800

    completed draft reference data pattern

--- scripts/build_dev.bat
+++ scripts/build_dev.bat
@@ -6,10 +6,10 @@ call docker-compose ^
 -f docker-compose.shared.admin.yml ^
 -f docker-compose.shared.env.yml ^
 -f docker-compose.shared.volumes.yml ^
+-f docker-compose.dev.volumes.yml ^
 -f docker-compose.dev.env.yml ^
 -f docker-compose.dev.ports.yml ^
 -f docker-compose.dev.build.yml ^
-
 config > docker-stack.yml
 
 docker-compose -f docker-stack.yml build
