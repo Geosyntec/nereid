d6b76820feadc8d1b751645c9001dcb49bb7ffb6
Austin Orr <AOrr@geosyntec.com>
2020-02-03 15:20:01 -0800

    cleanup docker compose; add deploy script

--- scripts/build_deploy.sh
+++ scripts/build_deploy.sh
@@ -6,10 +6,8 @@ docker-compose \
 -f docker-compose.shared.env.yml \
 -f docker-compose.shared.volumes.yml \
 -f docker-compose.shared.ports.yml \
--f docker-compose.edge.build.yml \
--f docker-compose.dev.volumes.yml \
--f docker-compose.dev.env.yml \
--f docker-compose.dev.ports.yml \
+-f docker-compose.shared.build.yml \
+-f docker-compose.deploy.images.yml \
 config > docker-stack.yml
 
 docker-compose -f docker-stack.yml build
