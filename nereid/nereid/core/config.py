f72e8ad5f72233296fa4752e2ae850cfcbe042b5
Austin Orr <AOrr@geosyntec.com>
2020-04-16 08:48:46 -0700

    add source code version and author info to config

--- nereid/nereid/core/config.py
+++ nereid/nereid/core/config.py
@@ -1,11 +1,20 @@
 import os
 from pathlib import Path
 
+import nereid
 from nereid.core.io import load_cfg
 
 API_V1_STR = "/api/v1"
 API_LATEST = API_V1_STR
 APP_CONTEXT = load_cfg(Path(__file__).parent / "base_config.yml")
+APP_CONTEXT.update(
+    {
+        "version": nereid.__version__,
+        "author": nereid.__author__,
+        "contact": nereid.__email__,
+    }
+)
+
 
 _NEREID_FORCE_FOREGROUND = os.getenv("NEREID_FORCE_FOREGROUND", "")
 NEREID_FORCE_FOREGROUND = _NEREID_FORCE_FOREGROUND not in ["", "0"]
