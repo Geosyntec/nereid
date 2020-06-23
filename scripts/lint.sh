3e849289c6b9442a1c4eb91025a83d10ebb22e54
Austin Orr <AOrr@geosyntec.com>
2020-06-23 13:17:33 -0700

    bump version and dependencies

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,6 +5,6 @@ set -x
 
 mypy --config-file=nereid/mypy.ini nereid/nereid
 black nereid --check
-isort \
---multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as \
---line-width 88 --recursive --check-only --thirdparty orjson nereid/nereid
+isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as \
+--line-width 88 --recursive --check-only --diff --thirdparty orjson \
+--thirdparty yaml nereid/nereid
