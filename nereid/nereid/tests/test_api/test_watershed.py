bb1234903a10fad3221c2c92ac1c25a87be858fb
Austin Orr <AOrr@geosyntec.com>
2020-07-06 14:50:12 -0700

    embed version in solution responses, bump version

--- nereid/nereid/tests/test_api/test_watershed.py
+++ nereid/nereid/tests/test_api/test_watershed.py
@@ -53,6 +53,8 @@ def test_get_solve_watershed(client, watershed_responses, size, pct_tmnt):
             errors = res.get("errors", "")
             assert "error" not in errors.lower()
 
+            assert res.get('_version') is not None
+
 
 def test_post_solve_watershed_stable(
     client, watershed_requests, watershed_responses, watershed_test_case
