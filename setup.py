92346b12652687b880995462c65c1ba56bf455f7
Austin Orr <AOrr@geosyntec.com>
2020-11-17 15:19:31 -0800

    make nereid installable; bump python version

---
+++ setup.py
@@ -0,0 +1,37 @@
+import re
+
+from setuptools import setup, find_packages
+
+with open("nereid/nereid/__init__.py", encoding="utf8") as f:
+    content = f.read()
+    version = re.search(r'__version__ = "(.*?)"', content).group(1)
+    author = re.search(r'__author__ = "(.*?)"', content).group(1)
+    author_email = re.search(r'__email__ = "(.*?)"', content).group(1)
+
+print(find_packages())
+
+
+setup(
+    version=version,
+    author=author,
+    author_email=author_email,
+    install_requires=[
+        "python-dotenv>=0.14,<0.15",
+        "scipy>=1.5",
+        "pandas>=1.1>1.2",
+        "networkx>=2.5,<2.6",
+        "pydot>=1.4,<1.5",
+        "graphviz>=0.14,<0.15",
+        "matplotlib>=3.3,<3.4",
+        "fastapi>=0.61,<0.62",
+        "pydantic>=1.7,<1.8",
+        "aiofiles>=0.6,<0.7",
+        "celery>=5.0,<5.1",
+        "jinja2>=2.11,<2.12",
+        "redis>=3.5,<3.6",
+        "orjson>=3.4,<3.5",
+        "pyyaml>=5.3,<5.4",
+        "pint>=0.16,<0.17",
+        "typing-extensions>=3.7,<3.8",
+    ],
+)
