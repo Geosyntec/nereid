6896440348b516b5e2839dca5ad83f68ba3b152c
Austin Orr <AOrr@geosyntec.com>
2021-07-04 13:00:43 -0700

    patch coverage

--- nereid/nereid/core/config.py
+++ nereid/nereid/core/config.py
@@ -30,7 +30,7 @@ class Settings(BaseSettings):
 
     @validator("ALLOW_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
-        if isinstance(v, str) and not v.startswith("["):  # pragma: no branch
+        if isinstance(v, str) and not v.startswith("["):  # pragma: no cover
             return [i.strip() for i in v.split(",")]
         elif isinstance(v, (list, str)):
             return v
