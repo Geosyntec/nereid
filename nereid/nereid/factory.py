252d93f7ea1923897f9064d4296255b3fdeca354
Austin Orr <AOrr@geosyntec.com>
2022-02-28 19:48:08 -0800

    enable mounted docs

--- nereid/nereid/factory.py
+++ nereid/nereid/factory.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, Optional
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
 from fastapi.staticfiles import StaticFiles
@@ -10,13 +10,29 @@ from nereid.api.api_v1.utils import get_valid_context
 from nereid.core.config import nereid_path, settings
 
 
-def create_app(**settings_override: Optional[Dict[str, Any]]) -> FastAPI:
+def create_app(
+    *,
+    settings_override: Optional[Dict[str, Any]] = None,
+    app_kwargs: Optional[Dict[str, Any]] = None
+) -> FastAPI:
 
     _settings = settings.copy(deep=True)
+    if settings_override is not None:  # pragma: no branch
     _settings.update(settings_override)
 
+    kwargs = {}
+    if app_kwargs is not None:  # pragma: no cover
+        kwargs = app_kwargs
+
+    _docs_url: Optional[str] = kwargs.pop("docs_url", None)
+    _redoc_url: Optional[str] = kwargs.pop("redoc_url", None)
+
     app = FastAPI(
-        title="nereid", version=_settings.VERSION, docs_url=None, redoc_url=None
+        title="nereid",
+        version=_settings.VERSION,
+        docs_url=_docs_url,
+        redoc_url=_redoc_url,
+        **kwargs
     )
     setattr(app, "_settings", _settings)
 
@@ -47,6 +63,8 @@ def create_app(**settings_override: Optional[Dict[str, Any]]) -> FastAPI:
         allow_headers=["*"],
     )
 
+    if app.docs_url is None:  # pragma: no branch
+
     @app.get("/docs", include_in_schema=False)
     async def custom_swagger_ui_html():
         return get_swagger_ui_html(
@@ -57,6 +75,8 @@ def create_app(**settings_override: Optional[Dict[str, Any]]) -> FastAPI:
             swagger_css_url="/static/swagger-ui.css",
             swagger_favicon_url="/static/logo/trident_neptune_logo.ico",
         )
+
+    if app.redoc_url is None:  # pragma: no branch
 
     @app.get("/redoc", include_in_schema=False)
     async def redoc_html():
