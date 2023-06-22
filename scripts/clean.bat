3127054ae78a9c12935d875a553b7a7b356c7d7c
Austin Orr <aorr@geosyntec.com>
2023-06-22 11:50:33 -0700

    rm windows dev support; use wsl

--- scripts/clean.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /d /r . %%i in (__pycache__) do @if exist "%%i" rd /s /q "%%i" && echo Removed "%%i"
-for /d /r . %%i in (.mypy_cache) do @if exist "%%i" rd /s /q "%%i" && echo Removed "%%i"
-for /d /r . %%i in (.pytest_cache) do @if exist "%%i" rd /s /q "%%i" && echo Removed "%%i"
-for /d /r . %%i in (.coverage) do @if exist "%%i" del /s /q "%%i"
--- scripts/make_dev_env.bat
+++
@@ -1,8 +0,0 @@
-call conda activate base
-call conda env remove -n nereid_dev
-call conda create -n nereid_dev python=3.8 jupyter
-call conda activate nereid_dev
-call pip install ^
-  -r nereid\requirements.txt ^
-  -r nereid\requirements_tests.txt
-call conda activate base
