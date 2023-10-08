1e5bcfa42e3b37c8eecf02a491295d66527d19fd
Austin Orr <aorr@geosyntec.com>
2023-10-08 15:46:50 -0700

    cleanup static docs

---
+++ nereid/nereid/api/docs.py
@@ -0,0 +1,20 @@
+from fastapi.openapi.docs import get_swagger_ui_html
+from starlette.responses import HTMLResponse
+
+
+# Swagger UI includes a supermassive curl div that is not height constrained.
+# Not sure how this hasn't been fixed, but this patches it for this project.
+def get_better_swagger_ui_html(*args, **kwargs) -> HTMLResponse:
+    body_lines = get_swagger_ui_html(**kwargs).body.decode().split("\n")
+    ix = 4
+    for i, line in enumerate(body_lines):
+        if 'type="text/css" rel="stylesheet"' in line:
+            ix = i
+
+    new_css = "<style>#swagger-ui .curl-command .curl {max-height: 20em;}</style>"
+
+    body_lines.insert(ix + 1, new_css)
+
+    html = "\n".join(body_lines)
+
+    return HTMLResponse(html)
