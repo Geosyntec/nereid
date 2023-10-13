3cacfcbbda0271cd7b9cc29bcc08f0cce04a8824
Austin Orr <aorr@geosyntec.com>
2023-10-13 13:32:30 -0700

    clean up build scripts

--- scripts/build_deploy.sh
+++ scripts/build_deploy.sh
@@ -2,7 +2,7 @@ set -e
 export COMPOSE_FILE=docker-stack.yml
 export COMPOSE_DOCKER_CLI_BUILD=1
 
-docker-compose \
+docker compose \
 -f docker-compose.shared.depends.yml \
 -f docker-compose.shared.env.yml \
 -f docker-compose.shared.volumes.yml \
