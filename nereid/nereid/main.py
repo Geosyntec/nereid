70d6853bf58a7f556786b7a21facd55b1248cb89
Austin Orr <aorr@geosyntec.com>
2023-11-01 18:02:53 -0700

    logger formatting

--- nereid/nereid/main.py
+++ nereid/nereid/main.py
@@ -2,6 +2,9 @@ from nereid.core.config import settings
 from nereid.core.log import logging
 from nereid.factory import create_app
 
-logging.basicConfig(level=settings.LOG_LEVEL)
+logging.basicConfig(
+    format="%(levelname)-9s %(name)s: %(message)s",
+    level=settings.LOG_LEVEL,
+)
 
 app = create_app()
