63d1ae2451f0291f409d145deab8e41df1c0377e
Austin Orr <aorr@geosyntec.com>
2024-01-29 17:06:34 -0800

    add entrypoint alias for uvicorn

--- nereid/nereid/main.py
+++ nereid/nereid/main.py
@@ -1,10 +1,48 @@
-from nereid.core.config import settings
-from nereid.core.log import logging
+import logging
+import logging.config
+
+from nereid.core.config import LOGGING_CONFIG
 from nereid.factory import create_app
 
-logging.basicConfig(
-    format="%(levelname)-9s %(name)s: %(message)s",
-    level=settings.LOG_LEVEL,
-)
-
 app = create_app()
+
+
+def main():
+    import argparse
+
+    import uvicorn
+    import uvicorn.config
+
+    from nereid.core.utils import merge_dicts
+
+    logging.config.dictConfig(config=LOGGING_CONFIG)
+    logger = logging.getLogger("nereid")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--host", help="Bind socket to this host.  [default: 127.0.0.1]", type=str
+    )
+    parser.add_argument(
+        "--port",
+        help="Bind socket to this port. If 0, an available port will be picked.  [default: 8000].",
+        type=int,
+    )
+    parser.add_argument(
+        "--reload", help="Enable auto-reload.", action="store_true", default=False
+    )
+
+    args, unk = parser.parse_known_args()
+    kwargs = {k: v for k, v in vars(args).items() if v is not None}
+
+    # logger.warning("warning message with extras", extra={"x": "hello"})
+
+    if unk:
+        logger.warning(f'ignored arguments: {" ".join(unk)}')
+
+    config = merge_dicts(uvicorn.config.LOGGING_CONFIG, LOGGING_CONFIG)
+
+    uvicorn.run("nereid.factory:create_app", factory=True, **kwargs, log_config=config)
+
+
+if __name__ == "__main__":
+    main()
