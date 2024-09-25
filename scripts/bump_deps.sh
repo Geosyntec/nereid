4ece000ef33f21d5c194cc61c4040f489cfad8bc
Austin Orr <aorr@geosyntec.com>
2024-09-25 16:59:51 -0700

    bump deps; drop black; uv compile

--- scripts/bump_deps.sh
+++ scripts/bump_deps.sh
@@ -2,51 +2,16 @@
 
 set -e
 
-cleanup_pip () {
-
-if [ ! -z "$(pip freeze)" ]
-then
-    echo "cleaning up"
-    pip uninstall -y -r <(pip freeze)
-    pip install uv
-fi
-
-}
-
-
-# setup env
-eval "$(conda shell.bash hook)"
-conda activate base
-conda remove -n nereid-install --all -y
-conda create -n nereid-install python=3.11 -y
-conda activate nereid-install
-pip install uv
-
-
-# start building deps
-cleanup_pip
+pip install -U "uv>=0.4,<0.5"
 
 # dev
-uv pip install -r nereid/requirements/requirements_dev_unpinned.txt
-uv pip freeze > nereid/requirements/requirements_dev.txt
-
-
-cleanup_pip
+uv pip compile nereid/requirements/requirements_dev_unpinned.txt > nereid/requirements/requirements_dev.txt
 
 # nereid
-uv pip install -r nereid/requirements/requirements_nereid_unpinned.txt
-uv pip freeze > nereid/requirements/requirements_nereid.txt
-
-cleanup_pip
+uv pip compile nereid/requirements/requirements_nereid_unpinned.txt > nereid/requirements/requirements_nereid.txt
 
 # worker
-uv pip install -r nereid/requirements/requirements_worker_unpinned.txt
-uv pip freeze > nereid/requirements/requirements_worker.txt
-
-cleanup_pip
+uv pip compile nereid/requirements/requirements_worker_unpinned.txt > nereid/requirements/requirements_worker.txt
 
 # lint
-uv pip install -r nereid/requirements/requirements_lint_unpinned.txt
-uv pip freeze > nereid/requirements/requirements_lint.txt
-
-cleanup_pip
+uv pip compile nereid/requirements/requirements_lint_unpinned.txt > nereid/requirements/requirements_lint.txt
