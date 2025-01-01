770ab55fff855a651dba6fc96c9dc1763cd1ec6a
Austin Orr <aorr@geosyntec.com>
2025-01-01 14:36:28 -0800

    test for frontend

--- nereid/nereid/tests/test_api/test_main.py
+++ nereid/nereid/tests/test_api/test_main.py
@@ -3,7 +3,12 @@ import pytest
 
 @pytest.mark.parametrize(
     "route, title_contents",
-    [("/docs", "swagger"), ("/redoc", "redoc"), ("/config", "test")],
+    [
+        ("/docs", "swagger"),
+        ("/redoc", "redoc"),
+        ("/config", "test"),
+        ("/app", "nereid ui"),
+    ],
 )
 def test_docs(client, route, title_contents):
     response = client.get(route)
